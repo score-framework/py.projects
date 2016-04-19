93dc2d0676271814ae665aa3c6057b5f26fbaac3
Necdet Can Ateşman <can@strg.at>
2016-04-19 11:17:38 +0200

    Release: 0.1.7

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ with open(os.path.join(here, 'README.rst')) as f:
 
 setup(
     name='score.projects',
-    version='0.1.6',
+    version='0.1.7',
     description='Manage your projects with The SCORE Framework',
     long_description=README,
     author='strg.at',
